2ccfe6d90983a07a9924ad1eb28d4b7de2ded044
Tiago Lima <tiago.jlima.developer@gmail.com>
2016-07-25 21:58:02 -0300

    inserindo página home e footer

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -3,44 +3,5 @@ angular.module('find-sampa')
     ['$scope','services',
     function($scope, services){
 
-  $scope.advertisers = [];
-  $scope.categories = [];
-
-  var listCategories = function(){
-    services.categoriesGet().query(function(categories) {
-      $scope.categories = categories;
-      console.log(categories)
-      for(var i = 0; i<categories.length; i++){
-        console.log(categories.length)
-        $scope.categories = categories[i];
-      }
-    });
-  };
-  listCategories();
-
-  var listAdvertisersFull = function(){
-    services.advertisersGetFull().query(function(advertisers){
-      $scope.advertisers = advertisers;
-      $scope.loading = '';
-    });
-  };
-  listAdvertisersFull();
-
-  var listAdvertisers = function(regiao){
-    services.advertisersGet(regiao).query(function(advertisers){
-      $scope.advertisers = advertisers;
-      $scope.loading = '';
-    });
-  };
-
-  $scope.regiao = function(regiao) {
-
-    $scope.loading = 'loading';
-
-    listAdvertisers(regiao);
-  };
-
-  $scope.loading = '';
-
 
 }]);
